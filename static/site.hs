6115cc7279da76180a8b594f7d3703beacfcb939
HarrisonCentner <harrison.centner@gmail.com>
2025-08-29 22:17:05 -0700

    html fixes

--- static/site.hs
+++ static/site.hs
@@ -6,131 +6,146 @@ import           Text.Pandoc.Options
 import           System.Environment (lookupEnv)
 import           Data.Maybe (isJust)
 import           Control.Monad
+-- import           System.FilePath
 
+{-
+robots, static, images, pdfs, css, imagesOrig, templates, diagramsOrig, cname, favicon, posts :: FilePath
+static = "static"
+images = static </> "images"
+pdfs = static </> "pdfs"
+css = static </> "css"
+imagesOrig = static </> "images-orig"
+templates = static </> "templates"
+diagramsOrig = static </> "diagrams-orig"
+cname = static </> "cname"
+favicon = static </> "favicon.ico"
+posts = static </> "posts"
+robots = static </> "robots.txt"
+-}
 
 --------------------------------------------------------------------------------
 main :: IO ()
-main =
-  do prod <- isJust <$> lookupEnv "PROD"
-     let myDefaultContext = mconcat
-                            [ boolField "prod" (const prod)
-                            , constField "root" root
-                            , defaultContext ]
-     hakyll $ do
-       match "images/*" $ do
-         route   idRoute
-         compile copyFileCompiler
+main = do 
+  prod <- isJust <$> lookupEnv "PROD"
+  let myDefaultContext = mconcat
+                         [ boolField "prod" (const prod)
+                         , constField "root" root
+                         , defaultContext ]
+  hakyll $ do
+    match "images/*" $ do
+      route   idRoute
+      compile copyFileCompiler
 
-       match "pdfs/*" $ do
-         route   idRoute
-         compile copyFileCompiler
+    match "pdfs/*" $ do
+      route   idRoute
+      compile copyFileCompiler
 
-       match "css/*" $ do
-         route   idRoute
-         compile compressCssCompiler
+    match "css/*" $ do
+      route   idRoute
+      compile compressCssCompiler
 
-       match (fromList ["CNAME", "favicon.ico", "robots.txt"]) $ do
-         route   idRoute
-         compile copyFileCompiler
+    match (fromList ["CNAME", "favicon.ico", "robots.txt"]) $ do
+      route   idRoute
+      compile copyFileCompiler
 
-       match "pages/*" $ do
-         route   $ gsubRoute "pages/" (const "") `composeRoutes` setExtension "html"
-         compile $ pandocCompiler
-           >>= loadAndApplyTemplate "templates/default.html" myDefaultContext
-           >>= relativizeUrls
+    match "pages/*" $ do
+      route   $ gsubRoute "pages/" (const "") `composeRoutes` setExtension "html"
+      compile $ pandocCompiler
+        >>= loadAndApplyTemplate "templates/default.html" myDefaultContext
+        >>= relativizeUrls
 
-       tags <- buildTags "posts/**" (fromCapture "tags/*.html")
-       let myPostCtx = mconcat
-                       [ dateField "date" "%B %e, %Y"
-                       , tagsField "tags" tags
-                       , myDefaultContext ]
-       tagsRules tags $ \tag pat -> do
-         route idRoute
-         compile $ do
-           posts <- recentFirst =<< loadAll pat
-           let myTagPageCtx = mconcat
-                 [ listField "posts" myPostCtx (return posts)
-                 , constField "title" $ "Posts tagged \"" ++ tag ++ "\""
-                 , boolField "noindex" (pure True)
-                 , myDefaultContext ]
+    tags <- buildTags "posts/**" (fromCapture "tags/*.html")
+    let myPostCtx = mconcat
+                    [ dateField "date" "%B %e, %Y"
+                    , tagsField "tags" tags
+                    , myDefaultContext ]
+    tagsRules tags $ \tag pat -> do
+      route idRoute
+      compile $ do
+        posts <- recentFirst =<< loadAll pat
+        let myTagPageCtx = mconcat
+              [ listField "posts" myPostCtx (return posts)
+              , constField "title" $ "Posts tagged \"" ++ tag ++ "\""
+              , boolField "noindex" (pure True)
+              , myDefaultContext ]
 
-           makeItem ""
-             >>= loadAndApplyTemplate "templates/tag.html" myTagPageCtx
-             >>= loadAndApplyTemplate "templates/default.html" myTagPageCtx
-             >>= relativizeUrls
+        makeItem ""
+          >>= loadAndApplyTemplate "templates/tag.html" myTagPageCtx
+          >>= loadAndApplyTemplate "templates/default.html" myTagPageCtx
+          >>= relativizeUrls
 
-       match "posts/**" $ do
-         route $ setExtension "html"
-         compile $ customPandocCompiler
-           >>= saveSnapshot "content"
-           >>= loadAndApplyTemplate "templates/post.html"    myPostCtx
-           >>= loadAndApplyTemplate "templates/default.html" myPostCtx
-           >>= relativizeUrls
+    match "posts/**" $ do
+      route $ setExtension "html"
+      compile $ customPandocCompiler
+        >>= saveSnapshot "content"
+        >>= loadAndApplyTemplate "templates/post.html"    myPostCtx
+        >>= loadAndApplyTemplate "templates/default.html" myPostCtx
+        >>= relativizeUrls
 
-       create ["archive.html"] $ do
-         route idRoute
-         compile $ do
-           posts <- recentFirst =<< loadAll "posts/**"
-           tagList <- renderTagList tags
-           let myArchiveCtx = 
-                 listField "posts" myPostCtx (return posts) `mappend`
-                 constField "taglist"  tagList             `mappend`
-                 constField "title" "Archives"            `mappend`
-                 myDefaultContext 
+    create ["archive.html"] $ do
+      route idRoute
+      compile $ do
+        posts <- recentFirst =<< loadAll "posts/**"
+        tagList <- renderTagList tags
+        let myArchiveCtx = 
+              listField "posts" myPostCtx (return posts) `mappend`
+              constField "taglist"  tagList             `mappend`
+              constField "title" "Archives"            `mappend`
+              myDefaultContext 
 
-           makeItem ""
-             >>= loadAndApplyTemplate "templates/archive.html" myArchiveCtx
-             >>= loadAndApplyTemplate "templates/default.html" myArchiveCtx
-             >>= relativizeUrls
+        makeItem ""
+          >>= loadAndApplyTemplate "templates/archive.html" myArchiveCtx
+          >>= loadAndApplyTemplate "templates/default.html" myArchiveCtx
+          >>= relativizeUrls
 
-       create ["sitemap.xml"] $ do
-         route   idRoute
-         compile $ do
-           posts <- recentFirst =<< loadAll "posts/*"
-           pages <- loadAll "pages/*"
-           let allPages = return (pages ++ posts)
-           let sitemapCtx = mconcat
-                            [ listField "pages" myPostCtx allPages
-                            , myDefaultContext
-                            ]
-           makeItem ""
-             >>= loadAndApplyTemplate "templates/sitemap.xml" sitemapCtx
-
-       create ["rss.xml"] $ do
-         route idRoute
-         compile $ do
-           let feedCtx = mconcat
-                         [ teaserField "teaser" "content"
-                         , bodyField "description"
-                         , myPostCtx
+    create ["sitemap.xml"] $ do
+      route   idRoute
+      compile $ do
+        posts <- recentFirst =<< loadAll "posts/*"
+        pages <- loadAll "pages/*"
+        let allPages = return (pages ++ posts)
+        let sitemapCtx = mconcat
+                         [ listField "pages" myPostCtx allPages
+                         , myDefaultContext
                          ]
-               absolutizeUrl u = if isExternal u then u else root ++ u
-           posts <- fmap (take 10) . recentFirst =<<
-             loadAllSnapshots "posts/*" "content"
-           processedPosts <- forM posts $
-             \p -> do pp <- loadAndApplyTemplate "templates/rss-description.html" feedCtx p
-                      return $ fmap (withUrls absolutizeUrl) pp
-           renderRss myFeedConfiguration feedCtx processedPosts
+        makeItem ""
+          >>= loadAndApplyTemplate "templates/sitemap.xml" sitemapCtx
 
-       match "index.html" $ do
-         route idRoute
-         compile $ do
-           posts <- fmap (take 5) . recentFirst =<< loadAllSnapshots "posts/*" "content"
-           let myTeaserPostCtx =
-                 teaserField "teaser" "content" <> myPostCtx
-               myIndexCtx = mconcat
-                            [ listField "posts" myTeaserPostCtx (return posts)
-                            , constField "canonical" (root ++ "/")
-                            , constField "homepage" "yes"
-                            , myDefaultContext ]
+    create ["rss.xml"] $ do
+      route idRoute
+      compile $ do
+        let feedCtx = mconcat
+                      [ teaserField "teaser" "content"
+                      , bodyField "description"
+                      , myPostCtx
+                      ]
+            absolutizeUrl u = if isExternal u then u else root ++ u
+        posts <- fmap (take 10) . recentFirst =<<
+          loadAllSnapshots "posts/*" "content"
+        processedPosts <- forM posts $
+          \p -> do pp <- loadAndApplyTemplate "templates/rss-description.html" feedCtx p
+                   return $ fmap (withUrls absolutizeUrl) pp
+        renderRss myFeedConfiguration feedCtx processedPosts
 
-           getResourceBody
-             >>= applyAsTemplate myIndexCtx
-             >>= loadAndApplyTemplate "templates/default.html" myIndexCtx
-             >>= relativizeUrls
+    match "index.html" $ do
+      route idRoute
+      compile $ do
+        posts <- fmap (take 5) . recentFirst =<< loadAllSnapshots "posts/*" "content"
+        let myTeaserPostCtx =
+              teaserField "teaser" "content" <> myPostCtx
+            myIndexCtx = mconcat
+                         [ listField "posts" myTeaserPostCtx (return posts)
+                         , constField "canonical" (root ++ "/")
+                         , constField "homepage" "yes"
+                         , myDefaultContext ]
 
-       match "about/*" $ compile templateBodyCompiler
-       match "templates/*" $ compile templateBodyCompiler
+        getResourceBody
+          >>= applyAsTemplate myIndexCtx
+          >>= loadAndApplyTemplate "templates/default.html" myIndexCtx
+          >>= relativizeUrls
+
+    match "about/*" $ compile templateBodyCompiler
+    match "templates/*" $ compile templateBodyCompiler
 
 --------------------------------------------------------------------------------
 
@@ -153,9 +168,9 @@ customPandocCompiler =
 
 myFeedConfiguration :: FeedConfiguration
 myFeedConfiguration = FeedConfiguration
-    { feedTitle       = "Thomas Read's blog"
-    , feedDescription = "My personal blog - mostly maths and programming"
-    , feedAuthorName  = "Thomas Read"
-    , feedAuthorEmail = "thjread@gmail.com"
+    { feedTitle       = "Harrison Centner's blog"
+    , feedDescription = "My blog --- programming, maths, and security"
+    , feedAuthorName  = "Harrison Centner"
+    , feedAuthorEmail = "hcentner@umich.edu"
     , feedRoot        = root
     }
